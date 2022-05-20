0e937bf825cf08a194a082620966978476a03945
HishamAfifi <hish.abdelshafouk@gmail.com>
2022-05-20 16:50:48 +0200

    ...

--- app.js
+++ app.js
@@ -7,17 +7,7 @@ const router = require("./routes/routes");
 
 app.use(express.json());
 
-const whitelist = ["http://localhost:4200", "http://localhost:3000"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
-app.use(cors({ ...corsOptions, credentials: true }));
+app.use(cors({ origin: "http://localhost:4200", credentials: true }));
 // app.use(cors());
 
 app.get("/", (req, res) => {
